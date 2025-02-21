ffa070db46b6a301d6cf1f94548b2836a3ce9d4b
Gibbs <mr.darmstadtiumgmail.com>
2025-02-21 17:01:11 +0100

    Rewrote code without using selenium

--- scrape_books/scrape_books/items.py
+++ scrape_books/scrape_books/items.py
@@ -2,11 +2,16 @@
 #
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
+from typing import Optional
 
 import scrapy
 
 
 class ScrapeBooksItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title: Optional[str] = scrapy.Field()
+    price: Optional[float] = scrapy.Field()
+    amount_in_stock: Optional[int] = scrapy.Field()
+    rating: Optional[str] = scrapy.Field()
+    category: Optional[str] = scrapy.Field()
+    description: Optional[str] = scrapy.Field()
+    upc: Optional[str] = scrapy.Field()
--- scrape_books/scrape_books/spiders/books.py
+++ scrape_books/scrape_books/spiders/books.py
@@ -1,20 +1,7 @@
-from pathlib import Path
-
 import scrapy
-from scrapy import Spider, Selector
 from scrapy.http import Response
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-
-from twisted.internet.defer import Deferred
-
-
-options = Options()
-
-options.add_argument("--headless")
-options.add_argument("--disable-gpu")
+from scrape_books.items import ScrapeBooksItem
 
 
 class BooksSpider(scrapy.Spider):
@@ -22,59 +9,38 @@ class BooksSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/"]
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.driver = webdriver.Chrome(options=options)
-
-    def close(self: Spider, reason: str) -> Deferred[None] | None:
-        self.driver.close()
-        return super().close(spider=self, reason=reason)
-
     def parse(self, response: Response, **kwargs):
         for product in response.css(".product_pod"):
 
             detail_book_url = response.urljoin(product.css(".image_container").css("a::attr(href)").get())
-            detailed_info = self._parse_detailed_info(detail_book_url=detail_book_url)
+            book = ScrapeBooksItem()
 
-            yield {
-                "title": product.css("h3").css("a::attr(title)").get(),
-                "price": float(product.css(".price_color::text").get()[1:]),
-                "amount_in_stock": detailed_info["amount_in_stock"],
-                "rating": detailed_info["rating"],
-                "category": detailed_info["category"],
-                "description": detailed_info["description"],
-                "upc": detailed_info["upc"]
-            }
+            yield response.follow(
+                detail_book_url,
+                callback=self.parse_single_book,
+                cb_kwargs={"book": book}
+            )
 
             next_page = response.css(".next").css("a::attr(href)").get()
+
             if next_page is not None:
                 yield response.follow(next_page, callback=self.parse)
 
-    def _parse_detailed_info(self, detail_book_url: str) -> dict:
-        self.driver.get(detail_book_url)
+    def parse_single_book(self, response: Response, book):
+        for mark in (".One", ".Two", ".Three", ".Four", ".Five"):
+            try:
+                response.css(mark)
+            except Exception:
+                rating = None
+            else:
+                rating = mark[1:]
 
-        amount_in_stock = int(self.driver.find_element(By.CLASS_NAME, "availability").text.split(" ")[-2][1:])
+        book["title"] = response.css(".product_main").css("h1::text").get(),
+        book["price"] = float(response.css(".price_color::text").get()[1:])
+        book["amount_in_stock"] = int(response.css(".availability").get().split(" ")[-6][1:])
+        book["category"] = response.css(".breadcrumb").css("a::text").getall()[2]
+        book["description"] = response.css("p").getall()[3]
+        book["upc"] = response.css(".table").css("td::text").get()
+        book["rating"] = rating
 
-        rating_element = self.driver.find_element(By.CLASS_NAME, "star-rating")
-        rating = rating_element.get_attribute("class").split(" ")[-1]
-
-        category = self.driver.find_element(
-            By.CLASS_NAME, "breadcrumb"
-        ).find_elements(
-            By.CSS_SELECTOR, "li"
-        )[-2].find_element(By.CSS_SELECTOR, "a").text
-
-        description = self.driver.find_elements(By.CSS_SELECTOR, "p")[3].text
-
-        upc = self.driver.find_element(
-            By.CLASS_NAME, "table"
-        ).find_element(By.CSS_SELECTOR, "td").text
-
-        detailed_info = {
-            "amount_in_stock": amount_in_stock,
-            "rating": rating,
-            "category": category,
-            "description": description,
-            "upc": upc
-        }
-        return detailed_info
+        yield book
